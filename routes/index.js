53b52015ca2d3329ad1baf8cb10fd3afc940457d
Vladyslav Byelozerskykh <vladorangeqwer@gmail.com>
2022-10-04 19:34:03 -0400

    Same as previous commit

--- routes/index.js
+++ routes/index.js
@@ -11,24 +11,24 @@ router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home'});
 });
 
-/* GET About Us page. */
+/* GET About Me page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('about', { title: 'About'});
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+  res.render('services', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('contact', { title: 'Contact'});
 });
 
 module.exports = router;
